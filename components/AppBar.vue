dbf056c1306a75b2b066dbadea90f13f4acb7d1a
Milosz <miloszkrainsky@gmail.com>
2023-04-19 19:55:00 +0200

    create navabr

---
+++ components/AppBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="fixed top-0 left-0 right-0 border-b border-white/20 bg-blur">
+    <nav class="container py-4 flex justify-between items-center">
+      <NuxtLink :to="{name: 'index'}" class="text-white text-2xl font-bold">LinkSnip</NuxtLink>
+      <ul class="flex items-center gap-4">
+        <li v-for="link in navLinks" :key="link.to">
+          <NuxtLink :to="link.to">{{ link.label }}</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink class="btn btn-primary">Sign In</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+const navLinks = ref([
+  {
+    to: '/',
+    label: 'Home'
+  },
+  {
+    to: '/about',
+    label: 'About'
+  },
+  {
+    to: '/contact',
+    label: 'Contact'
+  }
+])
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <AppBar />
+    <slot />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
